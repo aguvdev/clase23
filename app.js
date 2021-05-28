9968efaf382c8ce31435147259aef6f48dfab4bd
hannagustinvega <hannheitiragustin@hotmail.com>
2021-05-28 15:37:33 -0300

    Agregue una descr de npm install

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const app = express();// se pide la funcion de alto nivel llamado express
 const port = 3000;// saber donde esta corriendo tu host es importante
 
 app.listen(port, () => console.log('Servidor funcionando en el puerto '+ port));
+
+//cuando clonas un archivo sin node_module para instalarlo se usa: npm install
