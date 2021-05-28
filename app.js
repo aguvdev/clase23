b5ef9c090c5be1835de09544a9aa8740eeac94b6
hannagustinvega <hannheitiragustin@hotmail.com>
2021-05-28 15:23:44 -0300

    levanta servidor

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
-const app = express();
+const express = require('express');// siempre se requiere con el mismo nombre
+const app = express();// se pide la funcion de alto nivel llamado express
+const port = 3000;// saber donde esta corriendo tu host es importante
 
-app.listen(3000, () => console.log('Servidor funcionando en el puerto 3000'));
+app.listen(port, () => console.log('Servidor funcionando en el puerto '+ port));
